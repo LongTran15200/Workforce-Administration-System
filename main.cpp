09d4e44f6a3b9862e9f5c1e9e14372ccf78ade40
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2023-08-21 11:42:32 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include "Employee.h"
+using namespace std;
+
+int main() {
+	Employee e;
+	e.menu();
+	return 0;
+}
