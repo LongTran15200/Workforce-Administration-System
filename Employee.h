5479e3098bd550b70fd89022ffda8d6c50927d01
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-07-24 18:59:40 -0500

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,15 +1,19 @@
 #pragma once
-#include <string>
 
 class Employee {
 private:
-    int emp_id, sal, group_id,dob;
-    std::string name, address;
+    int emp_id, group_id;
+    long sal;
+    std::string name,dob,country,state,city;
+    
 
 public:
     void menu();
     void insert();
     void display();
     void search();
-    //void modify();
+    void modify();
+    void deletion();
+    void searGroup();
+    void showAlGroup();
 };
